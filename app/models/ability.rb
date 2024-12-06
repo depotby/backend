14eda2a8afd46a749d21aaf0fc639e7d24d7b57c
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-06 18:46:51 +0300

    add first abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,29 @@
 class Ability < ApplicationRecord
+  ABILITIES = %w[USER:READ
+                 USER:UPDATE
+                 ROLE:CREATE
+                 ROLE:READ
+                 ROLE:UPDATE
+                 ROLE:DELETE
+                 CATEGORY:CREATE
+                 CATEGORY:READ
+                 CATEGORY:UPDATE
+                 CATEGORY:DELETE
+                 CATEGORY_PROPERTY:CREATE
+                 CATEGORY_PROPERTY:READ
+                 CATEGORY_PROPERTY:UPDATE
+                 CATEGORY_PROPERTY:DELETE
+                 CATEGORY_PROPERTY_OPTION:CREATE
+                 CATEGORY_PROPERTY_OPTION:READ
+                 CATEGORY_PROPERTY_OPTION:UPDATE
+                 CATEGORY_PROPERTY_OPTION:DELETE].freeze
+
   has_many :role_abilities, dependent: :destroy
   has_many :roles, through: :role_abilities
+
+  validates :name, uniqueness: true
+
+  def self.available
+    ABILITIES
+  end
 end
