e9322b54175b6d675d4061808d24afd4a536f971
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-09 01:20:20 +0300

    refactor ttl for authentication tokens

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,14 +1,16 @@
 class Authentication < ApplicationRecord
+  TTL = Rails.configuration.application_settings[:authentication]
+
   belongs_to :user
 
   enum :status, inactive: 0, active: 1
   enum :access_type, regular: 0, admin: 1
 
-  generates_token_for :authorization, expires_in: 5.minutes do
+  generates_token_for :authorization, expires_in: TTL[:access_ttl] do
     [ status ]
   end
 
-  generates_token_for :refresh, expires_in: 1.month do
+  generates_token_for :refresh, expires_in: TTL[:refresh_ttl] do
     [ status ]
   end
 
--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,7 @@ module Depot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.application_settings = config_for(:application_settings)
   end
 end
