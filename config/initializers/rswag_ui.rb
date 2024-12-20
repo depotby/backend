f5df559281adfbe5a6f598835be411daf355557f
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-20 11:34:11 +0300

    fix deprecation message

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -7,8 +7,8 @@ Rswag::Ui.configure do |c|
   # (under openapi_root) as JSON or YAML endpoints, then the list below should
   # correspond to the relative paths for those endpoints.
 
-  c.swagger_endpoint "/swagger/v1/swagger.yaml", "API V1 Docs"
-  c.swagger_endpoint "/swagger/admin/swagger.yaml", "API Admin Docs"
+  c.openapi_endpoint "/swagger/v1/swagger.yaml", "API V1 Docs"
+  c.openapi_endpoint "/swagger/admin/swagger.yaml", "API Admin Docs"
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
