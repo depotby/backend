1a6a5b6ed49addc19faa97bee1d9dcc768103591
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-28 18:48:50 +0300

    add uniqueness validation for category property option model

--- app/models/category_property_option.rb
+++ app/models/category_property_option.rb
@@ -1,5 +1,6 @@
 class CategoryPropertyOption < ApplicationRecord
   belongs_to :category_property
 
-  validates :variant, presence: true
+  validates :variant, presence: true,
+            uniqueness: { case_sensitive: false, scope: :category_property }
 end
