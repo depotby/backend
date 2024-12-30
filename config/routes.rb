8bc1c3a02ee94cda1a1206991f0791b1b8da53f8
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-31 01:22:54 +0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,83 +8,83 @@ Rails.application.routes.draw do
 
   scope defaults: { format: :json } do
     namespace :admin do
-      controller :authentications do
-        get "authentications", action: :index
-        post "authentications", action: :create
-        post "authentications/refresh", action: :refresh
-        delete "authentications", action: :destroy
+      scope :authentications, controller: :authentications do
+        get "/", action: :index
+        post "/", action: :create
+        post "/refresh", action: :refresh
+        delete "/", action: :destroy
       end
 
-      controller :user do
-        get "user", action: :show
+      scope :user, controller: :user do
+        get "/", action: :show
       end
 
-      controller :users do
-        get "users", action: :index
-        get "users/:id", action: :show
+      scope :users, controller: :users do
+        get "/", action: :index
+        get "/:id", action: :show
       end
 
-      controller :roles do
-        get "roles", action: :index
-        post "roles", action: :create
-        get "roles/:id", action: :show
-        put "roles/:id", action: :update
-        delete "roles/:id", action: :destroy
-        post "roles/:id/abilities", action: :switch_ability
+      scope :roles, controller: :roles do
+        get "/", action: :index
+        post "/", action: :create
+        get "/:id", action: :show
+        put "/:id", action: :update
+        delete "/:id", action: :destroy
+        post "/:id/abilities", action: :switch_ability
       end
 
-      controller :categories do
-        get "categories", action: :index
-        post "categories", action: :create
-        get "categories/:id", action: :show
-        put "categories/:id", action: :update
-        delete "categories/:id", action: :destroy
+      scope :categories, controller: :categories do
+        get "/", action: :index
+        post "/", action: :create
+        get "/:id", action: :show
+        put "/:id", action: :update
+        delete "/:id", action: :destroy
       end
 
-      controller :category_properties do
-        get "categories/:category_id/properties", action: :index
-        post "categories/:category_id/properties", action: :create
-        get "categories/:category_id/properties/:property_id", action: :show
-        put "categories/:category_id/properties/:property_id", action: :update
-        delete "categories/:category_id/properties/:property_id", action: :destroy
+      scope "categories/:category_id/properties", controller: :category_properties do
+        get "/", action: :index
+        post "/", action: :create
+        get "/:property_id", action: :show
+        put "/:property_id", action: :update
+        delete "/:property_id", action: :destroy
       end
 
-      controller :category_property_options do
-        post "categories/:category_id/properties/:property_id/options", action: :create
-        put "categories/:category_id/properties/:property_id/options/:option_id", action: :update
-        delete "categories/:category_id/properties/:property_id/options/:option_id",
-               action: :destroy
+      scope "categories/:category_id/properties/:property_id/options",
+            controller: :category_property_options do
+        post "/", action: :create
+        put "/:option_id", action: :update
+        delete "/:option_id", action: :destroy
       end
 
-      controller :products do
-        get "products", action: :index
-        post "products", action: :create
-        get "products/:id", action: :show
-        put "products/:id", action: :update
-        delete "products/:id", action: :destroy
+      scope :products, controller: :products do
+        get "/", action: :index
+        post "/", action: :create
+        get "/:id", action: :show
+        put "/:id", action: :update
+        delete "/:id", action: :destroy
       end
     end
 
     namespace :v1 do
-      controller :user do
-        get "user", action: :show
-        post "user", action: :create
-        put "user", action: :update
+      scope :user, controller: :user do
+        get "/", action: :show
+        post "/", action: :create
+        put "/", action: :update
       end
 
-      controller :authentications do
-        get "authentications", action: :index
-        post "authentications", action: :create
-        post "authentications/refresh", action: :refresh
-        delete "authentications", action: :destroy
+      scope :authentications, controller: :authentications do
+        get "/", action: :index
+        post "/", action: :create
+        post "/refresh", action: :refresh
+        delete "/", action: :destroy
       end
 
-      controller :addresses do
-        get "addresses", action: :index
-        get "addresses/:id", action: :show
-        post "addresses", action: :create
-        put "addresses/:id", action: :update
-        delete "addresses/:id", action: :destroy
+      scope :addresses, controller: :addresses do
+        get "/", action: :index
+        get "/:id", action: :show
+        post "/", action: :create
+        put "/:id", action: :update
+        delete "/:id", action: :destroy
       end
     end
   end
