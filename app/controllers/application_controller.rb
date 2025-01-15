32bd0e761a58344d5590774c7f2d3de4b17b6032
Nikita Ignatovich <nikita@ignatovich.me>
2025-01-16 01:06:16 +0300

    setup locale on request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = locale_header if I18n.locale_available?(locale_header)
+  end
+
   def authorization
     Current.authentication = authentications_scope.find_by_token_for(:authorization,
                                                                      authorization_header)
@@ -8,6 +16,10 @@ class ApplicationController < ActionController::API
     render json: { status: 401,
                    error: I18n.t("controllers.application.authorization.unauthorized") },
            status: :unauthorized
+  end
+
+  def locale_header
+    request.headers["Accept-Language"]
   end
 
   def authorization_header
